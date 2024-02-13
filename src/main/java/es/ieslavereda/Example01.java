0129be90e4d8eec7caec9733e5145a09a56e94d7
xrosilloprofe <xv.rosillo@edu.gva.es>
2024-02-13 18:58:00 +0100

    20240213

--- src/main/java/es/ieslavereda/Example01.java
+++ src/main/java/es/ieslavereda/Example01.java
@@ -2,8 +2,17 @@ package es.ieslavereda;
 
 public class Example01 {
     public static void main(String[] args) {
+        decrementa(1);
+
         int x=73,y=0;
         int result = x/y;
         System.out.println("Result: " + result);
     }
+
+    public static int decrementa(int num) {
+        if(num==0)
+            return 0;
+        return decrementa(num);
+    }
+
 }
---
+++ src/main/java/es/ieslavereda/Example05.java
@@ -0,0 +1,34 @@
+package es.ieslavereda;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Example05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int div,x,y;
+        int[] vector = {0,1,2};
+
+        try{
+            System.out.print("Introduce el numerador: ");
+            x = scanner.nextInt();
+            System.out.print("Introduce el denominador: ");
+            y = scanner.nextInt();
+            div = x / y;
+            System.out.println("Resultado: " + div);
+
+
+
+        } catch (ArithmeticException e){
+            System.err.println("Error producido: " + e);
+        } catch (InputMismatchException e){
+            System.err.println("Vaya con las letras..." + e);
+            e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            System.out.println("FIN");
+        }
+
+    }
+}
